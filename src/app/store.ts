a4d48f32916d2ca8589ad65eb149e013186bb893
wangdong <wangdong@zhongan.io>
2020-12-27 19:49:52 +0800

    more

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,23 +2,39 @@ import { configureStore, getDefaultMiddleware, Action } from '@reduxjs/toolkit'
 import { applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'connected-react-router'
 import { ThunkAction } from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 import { createBrowserHistory, History } from 'history'
 import createRootReducer, { RootState } from './rootReducer'
 
 export const configuredStore = (history: History, initialState?: RootState) => {
-  const router = routerMiddleware(history)
-  const middleware = [...getDefaultMiddleware(), router]
+  const middleware = [...getDefaultMiddleware(), routerMiddleware(history)]
+
+  const excludeLoggerEnvs = ['test', 'production']
+  const shouldIncludeLogger = !excludeLoggerEnvs.includes(
+    process.env.NODE_ENV || ''
+  )
+
+  if (shouldIncludeLogger) {
+    const logger = createLogger({
+      level: 'info',
+      collapsed: true,
+    })
+    middleware.push(logger)
+  }
+
+  const rootReducer = createRootReducer(history)
   const store = configureStore({
-    reducer: createRootReducer(history),
+    reducer: rootReducer,
     middleware,
     preloadedState: initialState,
   })
 
   if (process.env.NODE_ENV === 'development' && module.hot) {
-    module.hot.accept('./rootReducer', () => {
-      const newRootReducer = require('./rootReducer').default
-      store.replaceReducer(newRootReducer)
-    })
+    module.hot.accept(
+      './rootReducer',
+      // eslint-disable-next-line global-require
+      () => store.replaceReducer(require('./rootReducer').default)
+    )
   }
 
   return store
